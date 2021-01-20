1e0d5fb08db3dc30b394ae333c1eef3b46d6ffaa
SeanKaat <seankaat@gmail.com>
2021-01-20 15:50:46 -0500

    change init and rename

--- setup.py
+++ setup.py
@@ -1,40 +1,22 @@
-from distutils.core import setup
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
-    name='kmeansclustering',         # How you named your package folder (MyLib)
-    packages=['kmeansclustering'],   # Chose the same as "name"
-    version='0.3',      # Start with a small number and increase it with every change you make
-    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    license='MIT',
-    # Give a short description about your library
-    description='kmeans constrained clustering',
+setuptools.setup(
+    name="ConstrainedKmeansCluster",  # Replace with your own username
+    version="0.0.4",
+    author="Sean Kaat",
+    author_email="seankaat@gmail.com",
+    description="A useable kmeans constrained clustering library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='Sean Kaat',                   # Type in your name
-    author_email='seankaat@gmail.com',      # Type in your E-Mail
-    # Provide either the link to your github or to your website
-    url='https://github.com/SeanKaat/kmeansclustering',
-    # I explain this later on
-    download_url='https://github.com/SeanKaat/kmeansclustering/archive/v_03.tar.gz',
-    # Keywords that define your package best
-    keywords=['Kmeans', 'clustering', 'constrained'],
-    install_requires=[
-        'pulp',
-        'argparse',
-    ],
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Development Status :: 3 - Alpha',
-        # Define that your audience are developers
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
