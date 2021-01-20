b901c761a09af6ffab3aab2668fc60bce8431a08
SeanKaat <seankaat@gmail.com>
2021-01-20 15:21:41 -0500

    fix setup and change init

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     # Provide either the link to your github or to your website
     url='https://github.com/SeanKaat/kmeansclustering',
     # I explain this later on
-    download_url='https://github.com/SeanKaat/kmeansclustering/archive/v_01.tar.gz',
+    download_url='https://github.com/SeanKaat/kmeansclustering/archive/v_02.tar.gz',
     # Keywords that define your package best
     keywords=['Kmeans', 'clustering', 'constrained'],
-    install_requires=[            # I get to this in a second
-        'validators',
-        'beautifulsoup4',
+    install_requires=[
+        'pulp',
+        'argparse',
     ],
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
@@ -27,7 +27,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         # Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
